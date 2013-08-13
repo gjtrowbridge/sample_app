37e6d853294ae9162261e9b48bd0db94342319c3
Greg <gjtrowbridge@gmail.com>
2013-08-12 18:22:50 -0700

    Add signin/signout capability with corresponding tests

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+    user = current_user
+    sign_out
+    if user
+      flash[:message] = "Goodbye, #{user.name}.  You have been successfully signed out."
+    end
+    redirect_to root_url
+  end
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -58,6 +58,8 @@ describe "Static pages" do
     expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
     expect(page).to have_title(full_title(''))
+    click_link "Sign in"
+    expect(page).to have_title(full_title('Sign in'))
   end
 
 end
